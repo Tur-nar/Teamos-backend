ff33d861140cf110eed6238e8dd591a9a7dd6cea
Tur-nar <timmyturnerrr50@gmail.com>
2026-07-28 22:37:45 +0100

    fix: taskGateway import issues in deployment

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { UploadModule } from '../upload/upload.module';
+import { TaskGateway } from '../../gateway/task.gateway';
 
 @Module({
   imports: [UploadModule],
   controllers: [TaskController],
-  providers: [TaskService],
+  providers: [TaskService, TaskGateway],
   exports: [TaskService]
 })
 export class TaskModule { }

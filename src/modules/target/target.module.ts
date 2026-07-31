62f310f18c79c5de5da3d5f8c56311bf489a8557
Tur-nar <timmyturnerrr50@gmail.com>
2026-07-31 01:26:48 +0100

    fix: taskgateway export

--- src/modules/target/target.module.ts
+++ src/modules/target/target.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TargetController } from './target.controller';
 import { TargetService } from './target.service';
 import { UploadModule } from '../upload/upload.module';
+import { TaskGateway } from 'src/gateway/task.gateway';
 
 @Module({
   imports: [UploadModule],
   controllers: [TargetController],
-  providers: [TargetService],
+  providers: [TargetService, TaskGateway],
   exports: [TargetService]
 })
 export class TargetModule { }
